2e4d503c558b7f5079510a341accd5d4af4b59eb
Pranay Sharma <121673814+impranaysharma@users.noreply.github.com>
2024-09-09 12:32:10 +0530

    Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rjshkhr/cleanfolio'
+      href='https://github.com/impranaysharma'
       className='link footer__link'
     >
       Created By Pranay Sharma
