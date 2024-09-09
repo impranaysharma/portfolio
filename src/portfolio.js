e147017df21937e6d6775d8b8f046299a1f750ab
Pranay Sharma <121673814+impranaysharma@users.noreply.github.com>
2024-09-09 12:37:05 +0530

    portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://impranaysharma.github.io/portfolio/',
+  homepage: 'https://github.com/impranaysharma',
   title: 'PS.',
 }
 
@@ -12,8 +12,8 @@ const about = {
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/impranaysharma/',
+    github: 'https://github.com/impranaysharma',
   },
 }
 
